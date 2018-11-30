8d6876020bf881bb6e02e254085fda6070b77343
LeshkaZaripov <45337753+LeshkaZaripov@users.noreply.github.com>
2018-11-30 18:27:40 +0500

    Update Calculations.java
    added  getPrice_Flat

--- src/Calculations.java
+++ src/Calculations.java
@@ -13,6 +13,16 @@ public class Calculations {
 	public final static double Tarif_Gas_House = 4994.02;
 	public final static double kommunalka = 0;
 
+// метод, возвращающий значение kommunalka для квартиры
+            public static double getPrice_Flat (double Flat_Size, double Hot_Water, double Cold_Water, double Gas_Flat,double Electricity) {
+            //объявляем kommunalka
+            	double kommunalka = 0;
 
+            //формула расчета для квартиры
+            	kommunalka =  Flat_Size*Tarif_Heating + Hot_Water*Tarif_Hot_Water + Cold_Water*Tarif_Cold_Water +
+            			Gas_Flat*Tarif_Gas_Flat + Electricity*Tarif_Electricity;
+            	kommunalka = Math.rint(100.0*kommunalka)/100.0;
+            	return (kommunalka);
+			}
 
 }
