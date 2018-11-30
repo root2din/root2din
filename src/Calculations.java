bb9f74d41f93099c7e4eb1273592570589c9f00e
faida888 <45388925+faida888@users.noreply.github.com>
2018-11-30 18:25:23 +0500

    Create Calculations.java

---
+++ src/Calculations.java
@@ -0,0 +1,18 @@
+/**
+ * Создаем класс, который будет выполнять расчет
+*/
+public class Calculations {
+	/**определяем тарифы по услугам.
+	 *
+	 */
+	public final static double Tarif_Hot_Water = 141.44;
+	public final static double Tarif_Cold_Water = 22.21;
+	public final static double Tarif_Heating = 37.47;
+	public final static double Tarif_Gas_Flat = 6.50;
+	public final static double Tarif_Electricity = 2.74;
+	public final static double Tarif_Gas_House = 4994.02;
+	public final static double kommunalka = 0;
+
+
+
+}
